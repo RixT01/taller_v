d54852f802c846a72b6b8de4aae5b0c7d5363a77
dvarelap <dan@nearsoft.com>
2019-03-29 12:36:55 -0700

    samples

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ include CarFactory
 # Database connection
 Database.load(db_file: 'db.yml')
 
-# Loads inventory
+# Which fields are available
 puts Inventory.continental_tires
 puts Inventory.michelin_tires
 puts Inventory.motors_with_turbo
@@ -14,3 +14,22 @@ puts Inventory.doors
 puts Inventory.manual_transmissions
 puts Inventory.automatic_transmissions
 
+# How to build a car object
+car = Car.new(
+  brand:        "Mercedes",
+  name:         "Citan",
+  tires:        "Continental",
+  motor:        "Turbo 2.5",
+  transmission: "Manual",
+  doors:        2,
+  style:        "Sedan"
+)
+
+# Post result to validator
+result = Transport.post_result(
+  team:       1,
+  total:      1,
+  cars:       [car]
+)
+
+puts result.body.inspect
